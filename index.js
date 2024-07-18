02875e864edc29dcb8403ff16cf99296b62934dd
UillSousa <uilassousa@yahoo.com.br>
2024-07-17 23:08:21 -0300

    Desafio Partidas Rankeadas DIO

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-asdasd;
+    function saldo(vitorias, derrotas){
+    return vitorias - derrotas
+};
+
+let resultado = saldo(30, 5);
+let Rank = " ";
+
+    if (resultado < 10)   
+        console.log("Ferro")    
+    else if(resultado >=11 && resultado <20){
+        Rank = "Bronze"
+    }else if(resultado >=11 && resultado <20){
+        Rank = "Prata"
+    }else if(resultado >=21 && resultado <50){
+        Rank = "Ouro"
+    }else if(resultado >=51 && resultado <80){
+        Rank = "Diamante"
+    }else if(resultado >=81 && resultado <90){
+        Rank = "Lendario"
+    }else if(resultado >=101){
+        Rank = "Imortal"
+    }
+
+    console.log("O Heroi tem o saldo de " + resultado, "esta no nivel de " + Rank
+    )
+
+    
+
+
