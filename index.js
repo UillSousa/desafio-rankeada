52e6b45fd79fdd61c4b8fd5ee08a4342f0529844
UillSousa <uilassousa@yahoo.com.br>
2024-07-17 23:18:21 -0300

    Desafio Partidas Rankeadas DIO

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ let Rank = " ";
 
     if (resultado < 10)   
         Rank = "Ferro"    
-    else if(resultado >=11 && resultado <20){
+    else if(resultado >=11 && resultado <=20){
         Rank = "Bronze"
-    }else if(resultado >=11 && resultado <20){
+    }else if(resultado >=21 && resultado <=50){
         Rank = "Prata"
-    }else if(resultado >=21 && resultado <50){
+    }else if(resultado >=51 && resultado <=80){
         Rank = "Ouro"
-    }else if(resultado >=51 && resultado <80){
+    }else if(resultado >=81 && resultado <=90){
         Rank = "Diamante"
-    }else if(resultado >=81 && resultado <90){
+    }else if(resultado >=91 && resultado <=100){
         Rank = "Lendario"
     }else if(resultado >=101){
         Rank = "Imortal"
